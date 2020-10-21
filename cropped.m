8cc96cd5af0fe36ea4a0baaa542cc62fafff5097
awrossi <73212309+awrossi@users.noreply.github.com>
2020-10-21 14:20:15 -0700

    Add files via upload

--- cropped.m
+++ cropped.m
@@ -11,11 +11,13 @@ avg = zeros(m*n,1);  % the average face
 A = [];
 
 count=0;
-D = 'yalefaces_uncropped/yalefaces';
-S=dir(fullfile(D,'subject*.*'));
+D = 'CroppedYale';
+P=dir(fullfile(D,'yale*'));
+for ii=1:numel(P)
+S=dir(fullfile(D,P(ii).name,'yale*.pgm'));
 for k = 1:numel(S)
     figure(1)
-    ff = fullfile(D,S(k).name);
+    ff = fullfile(D,P(ii).name,S(k).name);
     u = imread(ff);
     u = imresize(u,[m,n]);
     imshow(u)
@@ -30,15 +32,15 @@ for k = 1:numel(S)
     avg = avg + R;
     count = count + 1;
 end
+end
 
-%SVD Analysis and Single Value Spectrum
+%SVD Analysis and Single Value Spectrum 
 [U,S,V] = svd(A);
 rank(A)
 rank(S)
 figure(2)
 phi=diag(S);
 subplot(1,1,1), semilogy(phi,'ko')
-axis([0 155 10^(0) 10^(6)])
 
 %% Calculate the "averaged" face
 avg= avg/count;
@@ -55,14 +57,15 @@ end
 % Reconstruction based on Rank
 image=111;
 figure(5)
-f=[1 5 10 20 50 100 200 1000];
-for h=1:8
+f=[1 5 10 20 50 100 200 500 1000];
+for h=1:9
     reface = U(:,1:f(h))*S(1:f(h),1:f(h))*V(:,1:f(h)).';
     ax(h) = subplot(3,3,h);
     imshow( mat2gray( reshape( reface(:,image),[m n])));
     position = ['Rank: ' num2str(f(h))];
     xlabel(position);
 end
+
 
 
 
